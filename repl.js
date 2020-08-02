15c966f0073517f7b8cc926fbb949b770f3a218f
Toni Tienda <tienda@libra.co.jp>
2020-08-02 11:30:48 +0900

    Serialize row to be inserted

--- repl.js
+++ repl.js
@@ -5,6 +5,34 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const TableConfig = {
+    ID_SIZE: 4,
+    ID_OFFSET: 0,
+    USERNAME_SIZE: 32,
+    USERNAME_OFFSET: 4,
+    EMAIL_SIZE: 255,
+    EMAIL_OFFSET: 36,
+    ROW_SIZE: 291
+}
+
+const serializeRow = data => {
+    const buffer = Buffer.alloc(291);
+    buffer.writeInt32BE(data.id, TableConfig.ID_OFFSET)
+    buffer.write(data.username, TableConfig.USERNAME_OFFSET)
+    buffer.write(data.email, TableConfig.EMAIL_OFFSET)
+
+    return buffer
+}
+
+const deserializeRow = data => {
+    return {
+        id: data.slice(TableConfig.ID_OFFSET, TableConfig.ID_SIZE),
+        username: data.slice(TableConfig.USERNAME_OFFSET, TableConfig.USERNAME_SIZE),
+        email: data.slice(TableConfig.EMAIL_OFFSET, TableConfig.EMAIL_SIZE)
+    }
+}
+
+
 const MetaCommands = {
     '.exit': () => { rl.close(); process.exit(0) }
 }
@@ -18,21 +46,50 @@ function executeStatement(statement) {
     console.log('Executed.')
 }
 
-const StatemenNames = [
+function ExecuteInsert(args) {
+    if (args.length !== 3) {
+        console.log(`Number of arguments for insert are not correct.`)
+        return
+    }
+
+    console.log(`Inserting:`, args)
+    const row = {
+        id: Number(args[0]),
+        username: args[1],
+        email: args[2]
+    }
+
+    const serializedRow = serializeRow(row)
+    console.log(bufferToHex(serializedRow))
+
+}
+
+
+function bufferToHex(buffer) {
+    return [...new Uint8Array(buffer)]
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join(" ");
+}
+
+function ExecuteSelect(args) {
+    console.log(`Selecting:`, args)
+}
+
+const StatementNames = (input) => [
     {
         type: 'insert',
-        execute: () => { console.log('This is where we would do an insert.') },
+        execute: () => ExecuteInsert(input.split(' ').slice(1)),
         regex: /insert.*/i
     },
     {
         type: 'select',
-        execute: () => { console.log('This is where we would do a select.') },
+        execute: () => ExecuteSelect(input.split(' ').slice(1)),
         regex: /select.*/i
     }
 ]
 
 function prepareStatement(input) {
-    const statement = StatemenNames.find(s => s.regex.test(input))
+    const statement = StatementNames(input).find(s => s.regex.test(input))
 
     if (!statement) {
         console.log(`Unrecognized command at start of ${input}`)
